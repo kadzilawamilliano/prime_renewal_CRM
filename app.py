7cce19a7ec1c842dd8f9d929bacfd043f1b565a7
kadzilawamilliano <kadzilawamilliano15@gmail.com>
2026-07-10 00:54:25 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,14 @@
-from clients_actions import client_buttons
+from client_actions import client_buttons
+import pandas as pd
+import streamlit as st
 
-client_buttons(row)
+# Load your clients
+clients = pd.read_excel("motor_renewals_tracking.xlsx")
+
+# Display buttons for each client
+for index, row in clients.iterrows():
+
+    st.write(row["policy_holder"])
+    st.write(row["vehicle_reg"])
+
+    client_buttons(row)
