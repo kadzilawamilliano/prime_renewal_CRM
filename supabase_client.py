b009a4df3f1aa5f73d7ca79481ce1d312aa02ca2
kadzilawamilliano <kadzilawamilliano15@gmail.com>
2026-07-24 21:17:31 +0200

    Update SUPABASE_URL to use full URL

--- supabase_client.py
+++ supabase_client.py
@@ -1,7 +1,7 @@
 from supabase import create_client, Client
 import streamlit as st
 
-SUPABASE_URL = st.secrets["youhswjymbopblwzmwbhmxk"]
+SUPABASE_URL = st.secrets["https://hswjymbopblwzmwbhmxk.supabase.co"]
 SUPABASE_KEY = st.secrets["sb_publishable_X0nGEGiieiPklP67_A-xEQ_NENBgLHG"]
 
 supabase: Client = create_client(
