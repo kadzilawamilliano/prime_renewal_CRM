91dc76a83d9782ea18d5d6e0bda24f19d52b8f90
kadzilawamilliano <kadzilawamilliano15@gmail.com>
2026-07-24 14:39:38 +0200

    Update supabase_client.py

--- supabase_client.py
+++ supabase_client.py
@@ -1,7 +1,8 @@
 from supabase import create_client, Client
+import streamlit as st
 
-SUPABASE_URL = "hswjymbopblwzmwbhmxk"
-SUPABASE_KEY = "sb_publishable_X0nGEGiieiPklP67_A-xEQ_NENBgLHG"
+SUPABASE_URL = st.secrets["youhswjymbopblwzmwbhmxk"]
+SUPABASE_KEY = st.secrets["sb_publishable_X0nGEGiieiPklP67_A-xEQ_NENBgLHG"]
 
 supabase: Client = create_client(
     SUPABASE_URL,
