52834ee60ee42a9f96f1861effe7a47c3101ac1b
kadzilawamilliano <kadzilawamilliano15@gmail.com>
2026-07-24 21:56:06 +0200

    Replace Supabase secrets with hardcoded values
    Replaced secret keys with hardcoded values for Supabase URL and key.

--- supabase_client.py
+++ supabase_client.py
@@ -1,8 +1,8 @@
 from supabase import create_client, Client
 import streamlit as st
 
-SUPABASE_URL = st.secrets["https://hswjymbopblwzmwbhmxk.supabase.co"]
-SUPABASE_KEY = st.secrets["sb_publishable_X0nGEGiieiPklP67_A-xEQ_NENBgLHG"]
+SUPABASE_URL = "https://hswjymbopblwzmwbhmxk.supabase.co"
+SUPABASE_KEY = "sb_publishable_X0nGEGiieiPklP67_A-xEQ_NENBgLHG"]
 
 supabase: Client = create_client(
     SUPABASE_URL,
