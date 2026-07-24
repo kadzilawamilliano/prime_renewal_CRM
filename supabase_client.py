f4700adcde3bdefc0a0b032f11719dd36d95e04d
kadzilawamilliano <kadzilawamilliano15@gmail.com>
2026-07-24 21:57:26 +0200

    Fix syntax error in SUPABASE_KEY assignment

--- supabase_client.py
+++ supabase_client.py
@@ -2,7 +2,7 @@ from supabase import create_client, Client
 import streamlit as st
 
 SUPABASE_URL = "https://hswjymbopblwzmwbhmxk.supabase.co"
-SUPABASE_KEY = "sb_publishable_X0nGEGiieiPklP67_A-xEQ_NENBgLHG"]
+SUPABASE_KEY = "sb_publishable_X0nGEGiieiPklP67_A-xEQ_NENBgLHG"
 
 supabase: Client = create_client(
     SUPABASE_URL,
